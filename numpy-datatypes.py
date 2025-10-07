a71607c211a556e22771aa55e821afe6c4fe9155
aadhavan <gooppoitech@gmail.com>
2025-10-07 12:27:21 +0530

    Update numpy-datatypes.py

--- numpy-datatypes.py
+++ numpy-datatypes.py
@@ -34,3 +34,12 @@ import numpy as np
 arr = np.array([1, 2, 3, 4])
 
 print(arr.dtype)
+
+Example
+Get the data type of an array containing strings:
+
+import numpy as np
+
+arr = np.array(['apple', 'banana', 'cherry'])
+
+print(arr.dtype)
